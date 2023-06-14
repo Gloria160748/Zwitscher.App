efa0fbe0aa3911ffa74588ebfe34be23e05fd0dd
gmnie <gmnie@LAPTOP-JNLHI5F4>
2023-06-14 10:15:01 +0200

    Projektdateien hinzufügen.

---
+++ Zwitscher/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Zwitscher
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage())
+            {
+                BarBackgroundColor = Color.White,
+                BarTextColor = Color.FromHex("#27408b")
+            };
+           
+        }
+            
+        
+        
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Zwitscher/Pages/Startseite/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Zwitscher
+{
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Pages.Profil());
+        }
+
+        private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Login());
+        }
+
+        private void ToolbarItem_Clicked_2(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
